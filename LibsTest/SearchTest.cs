b11216bc0ee16f115ba4915c795eeaff1a45ac4d
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-12 09:48:36 +0100

    add test search file

--- LibsTest/SearchTest.cs
+++ LibsTest/SearchTest.cs
@@ -4,9 +4,28 @@ namespace LibsTest;
 
 public class SearchTest
 {
+    private static readonly string FileSearch =
+        "C:\\Users\\ZP6177\\Creative Cloud Files\\Programmation\\C#\\Ineo Infracom\\CompileCsv\\LibsTest\\FileTest\\Search";
+    
     [Fact]
-    private void SearchWorkerTest()
+    private void SearchWorkerWithNoSubDirTest()
     {
-        var worker = new SearchWorker();
+        var worker = new SearchWorker(FileSearch, false);
+        worker.FindAll().Wait();
+
+        var files = worker.GetResults();
+        
+        Assert.Equal(32, files.Count);
+    }
+    
+    [Fact]
+    private void SearchWorkerWithSubDirTest()
+    {
+        var worker = new SearchWorker(FileSearch, true);
+        worker.FindAll().Wait();
+
+        var files = worker.GetResults();
+        
+        Assert.Equal(96, files.Count);
     }
 }
