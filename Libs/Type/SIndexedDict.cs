10123b7caefb9ee4cdc2e6e6edb55e2c52f19e03
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-12 11:18:40 +0100

    create type

---
+++ Libs/Type/SIndexedDict.cs
@@ -0,0 +1,8 @@
+﻿namespace Libs.Type;
+
+public struct SIndexedDict
+{
+    public int Index { get; set; }
+    public IEnumerable<Dictionary<string, string>> Dictionary { get; set; }
+    public string FilePath { get; set; }
+}
