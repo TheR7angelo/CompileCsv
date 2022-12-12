3fdbd1f88f3b235f6655f864ba9f202669b06891
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-12 15:46:51 +0100

    valid writer function

---
+++ Libs/Csv/Writer.cs
@@ -0,0 +1,29 @@
+﻿namespace Libs.Csv;
+
+public class Writer
+{
+    private IEnumerable<Dictionary<string, string>> Datas { get; }
+
+    public Writer(IEnumerable<Dictionary<string, string>> datas)
+    {
+        Datas = datas;
+    }
+
+    public void Write(string filePath, string separator=";")
+    {
+        using var file = new StreamWriter(filePath);
+        
+        var keys = Datas.First().Keys;
+
+        var header = string.Join(separator, keys);
+        file.WriteLine(header);
+
+        foreach (var data in Datas)
+        {
+            var values = string.Join(separator, data.Values);
+            file.WriteLine(values);
+        }
+        file.Flush();
+        file.Close();
+    }
+}
