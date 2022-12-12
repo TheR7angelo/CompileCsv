993d96336573451121d25d41688db4c4e7ff4211
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-12 11:18:25 +0100

    vaild test

---
+++ LibsTest/Csv/ReaderTest.cs
@@ -0,0 +1,38 @@
+﻿using Libs;
+using Libs.Csv;
+
+namespace LibsTest.Csv;
+
+public class ReaderTest
+{
+    [Fact]
+    private void ReadAllOneTest()
+    {
+        const string testFile =
+            "C:\\Users\\ZP6177\\Creative Cloud Files\\Programmation\\C#\\Ineo Infracom\\CompileCsv\\LibsTest\\FileTest\\Search\\Classeur1.csv";
+        var worker = new Reader(testFile);
+
+        worker.ReadAll().Wait();
+        var result = worker.GetResult().ToList();
+        
+        Assert.Equal(8, result[0].Dictionary.Count());
+    }
+    
+    [Fact]
+    private void ReadAllMultipleTest()
+    {
+        const string searchPath =
+            "C:\\Users\\ZP6177\\Creative Cloud Files\\Programmation\\C#\\Ineo Infracom\\CompileCsv\\LibsTest\\FileTest\\Search";
+        var search = new SearchWorker(searchPath, false);
+        search.FindAll().Wait();
+        var files = search.GetResults();
+        
+        var worker = new Reader(files);
+
+        worker.ReadAll().Wait();
+        var result = worker.GetResult().ToList();
+        
+        Assert.Equal(32, result.Count);
+    }
+    
+}
