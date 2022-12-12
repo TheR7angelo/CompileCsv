49aa504ad90bc691fc32ceb16ad1691a15eeb408
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-12 13:41:59 +0100

    add check header file

--- Libs/ComplileWorker.cs
+++ Libs/ComplileWorker.cs
@@ -1,6 +1,31 @@
-﻿namespace Libs;
+﻿using Libs.Type;
+
+namespace Libs;
 
 public class ComplileWorker
 {
+    private List<string>? ErrorHeader { get; set; }
     
+    private List<SIndexedDict> Datas { get; }
+
+    public ComplileWorker(IEnumerable<SIndexedDict> datas)
+    {
+        ErrorHeader = new List<string>();
+        Datas = datas.OrderBy(s => s.Index).ToList();
+    }
+
+    public async Task CheckHeader()
+    {
+        var mainHeader = Datas.First().Dictionary.First().Keys;
+
+        await Parallel.ForEachAsync(Datas.Skip(1), (data, _) =>
+        {
+            var keys = data.Dictionary.First().Keys;
+            if (!mainHeader.SequenceEqual(keys)) ErrorHeader?.Add(data.FilePath);
+            
+            return default;
+        });
+    }
+
+    public IEnumerable<string>? GetErrorHeader() => ErrorHeader.Count.Equals(0) ? null : ErrorHeader;
 }
