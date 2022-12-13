4433c8b0247d2c87f321aecf2f716eae8f279252
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-13 15:58:15 +0100

    clean code

--- CompileCsv/Views/MainView.xaml.cs
+++ CompileCsv/Views/MainView.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 using CompileCsv.Type;
+using Libs;
+using Libs.Csv;
+using Libs.Type;
 
 namespace CompileCsv.Views;
 
@@ -12,6 +18,29 @@ public partial class MainView
         
         SearchFiles.SearchFilesOnFindFiles += SearchFiles_OnFindFiles;
     }
+
+    #region Actions
+
+    private async void ButtonCheckFile_OnClick(object sender, RoutedEventArgs e)
+    {
+        var files = await ReadFiles();
+        var check = new CompileWorker(files);
+        await check.CheckHeader();
+        var errors = check.GetErrorHeader();
+
+        if (errors is null) return;
+
+        var data = GetFiles().ToList();
+        foreach (var error in errors)
+        {
+            var p =data.First(s => s.Path.Equals(error));
+            p.IsChecked = false;
+        }
+        ListBoxFiles.Items.Refresh();
+        CountSelectedFile();
+    }
+    
+    private void CheckBoxFile_OnClick(object sender, RoutedEventArgs e) => CountSelectedFile();
 
     private void SearchFiles_OnFindFiles(IEnumerable<string> files)
     {
@@ -28,5 +57,26 @@ public partial class MainView
         }
 
         ListBoxFiles.ItemsSource = data;
+        LabelNumberFind.Content = data.Count;
+        CountSelectedFile();
     }
+
+    #endregion
+
+    #region Function
+
+    private void CountSelectedFile() => LabelNumberSelected.Content = GetFiles().Count(s => s.IsChecked);
+
+    private IEnumerable<SelectedFile> GetFiles() => ListBoxFiles.Items.Cast<SelectedFile>();
+    
+    private async Task<IEnumerable<SIndexedDict>> ReadFiles()
+    {
+        var data = GetFiles();
+        var reader = new Reader(data.Where(s => s.IsChecked).Select(s => s.Path));
+        await reader.ReadAll();
+        return reader.GetResult();
+    }
+
+    #endregion
+    
 }
