82ecba4e93d0e3a98618c9e1df9d0a11375fe0b8
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-13 14:59:03 +0100

    init template

---
+++ CompileCsv/Views/MainView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CompileCsv.Type;
+
+namespace CompileCsv.Views;
+
+public partial class MainView
+{
+    public MainView()
+    {
+        InitializeComponent();
+        
+        SearchFiles.SearchFilesOnFindFiles += SearchFiles_OnFindFiles;
+    }
+
+    private void SearchFiles_OnFindFiles(IEnumerable<string> files)
+    {
+        var data = new List<SelectedFile>();
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileName(file);
+            data.Add(new SelectedFile
+            {
+                IsChecked = true,
+                Name = fileName,
+                Path = file
+            });
+        }
+
+        ListBoxFiles.ItemsSource = data;
+    }
+}
