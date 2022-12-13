e6e3c3a03117dfdbf7afff0d5e89be9f672a2e92
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-13 14:57:53 +0100

    add new type

---
+++ CompileCsv/Type/SelectedFile.cs
@@ -0,0 +1,12 @@
+﻿namespace CompileCsv.Type;
+
+public class SelectedFile
+{
+    public bool IsChecked { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Path { get; set; } = string.Empty;
+    
+    public SelectedFile()
+    {
+    }
+}
