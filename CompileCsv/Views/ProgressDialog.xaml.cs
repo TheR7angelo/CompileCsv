778a7cb1c16835ff71b8409fee04e1f62a696fb5
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-15 10:20:56 +0100

    add show progress

---
+++ CompileCsv/Views/ProgressDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Libs.Csv;
+
+namespace CompileCsv.Views;
+
+public partial class ProgressDialog
+{
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+    
+    
+    private IEnumerable<Dictionary<string, string>> CsvData { get; }
+    private string SavePath { get; }
+
+    public ProgressDialog(IEnumerable<Dictionary<string, string>> csvData, string savePath)
+    {
+        CsvData = csvData;
+        SavePath = savePath;
+        InitializeComponent();
+    }
+
+    private async void StartWork()
+    {
+        var progress = new Progress<float>(percent => ProgressBar.Value = percent);
+        var writer = new Writer(CsvData);
+        await Task.Run(() => writer.Write(SavePath, ";", progress), _cancellationTokenSource.Token);
+        Close();
+    }
+    
+    private void ProgressDialog_OnClosing(object? sender, CancelEventArgs e) => _cancellationTokenSource.Cancel();
+
+    private void ProgressDialog_OnActivated(object? sender, EventArgs e) => StartWork();
+}
