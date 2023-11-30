0d0aa9e1f37e47c4f9d08e399511aefae47d76f2
TheR7angelo <raphael.antunes@hotmail.fr>
2023-11-30 13:49:11 +0100

    ci(Update): Clean

--- Libs/Type/SIndexedDict.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Libs.Type;
-
-public struct SIndexedDict
-{
-    public int Index { get; set; }
-    public IEnumerable<Dictionary<string, string>> Dictionary { get; set; }
-    public string FilePath { get; set; }
-}
