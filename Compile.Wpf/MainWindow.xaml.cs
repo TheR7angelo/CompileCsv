b15fc8be55084fdade8adbe07af0415afe6d546d
TheR7angelo <raphael.antunes@hotmail.fr>
2023-11-30 14:04:54 +0100

    ci(Update): Start Ui

---
+++ Compile.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Compile.Wpf;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MenuItemUpdate_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void MenuItemChangelog_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Compile.Wpf/Pages/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Compile.Wpf.Pages;
+
+public partial class MainPage : Page
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
